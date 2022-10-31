880a1793e11e8b6c6bec1a2cf11568b9d9af7598
Abdus Samad <abdus.samad@xicom.biz>
2022-10-31 16:57:58 +0530

    connected mongoDb atlas and added colors package for console

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
 require("dotenv").config();
 const express = require("express");
+require("colors");
+
+const connectDB = require("./config/db");
 const chats = require("./data/data");
 
 const app = express();
+connectDB();
 app.get("/api/chats", (req, res) => res.send(chats));
 app.get("/api/chats/:id", (req, res) => {
   const singleChat = chats.find((el) => el?._id === req.params.id);
   res.send(singleChat);
 });
 
-app.listen(process.env.port || 5000, () =>
-  console.log(`server is running at port ${process.env.port || 5000}`)
+app.listen(
+  process.env.port || 5000,
+  console.log(`server is running at port ${process.env.port || 5000}`.yellow)
 );
